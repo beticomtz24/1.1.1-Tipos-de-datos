5527bca19263e8fb97494687ac7a5e6053540987
Alberto Martinez <albertomartinez@MacBook-Air-de-Alberto.local>
2025-02-10 16:59:17 -0600

    Upadte

---
+++ datos/main.js
@@ -0,0 +1,27 @@
+//Este es mi primero script en JavaScript
+console.log(typeof 5);
+console.log(5);
+
+
+console.log("Hola mundo");
+console.log(typeof "Hola mundo");
+
+/*Tipos de datos en JavaScript
+es importante conocerlos*/
+
+console.log(true);
+console.log(typeof true);
+
+console.log(5 + "5");
+
+//Para iniciar una varible, debemos iniciar con let
+
+let nombre = "Diego";
+
+console.log("Hola " + nombre);
+
+let numeroespecial = 5;
+
+let numeroespecialtexto = "5";
+
+console.log (numeroespecial + parseInt(numeroespecialtexto));
